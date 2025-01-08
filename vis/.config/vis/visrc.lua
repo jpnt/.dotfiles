cd1573510c88ab4308861d59976089d22ad0ff8e
Joao Pinto <joaopintojpgp@gmail.com>
2025-01-08 21:57:36 +0000

    remove submodule, bootstrap vis-plugged instead

--- vis/.config/vis/visrc.lua
+++ vis/.config/vis/visrc.lua
@@ -1,14 +1,18 @@
 require("vis")
-local plugged = require("plugins/vis-plugged")
+local plugged = (function() if not pcall(require, "plugins/vis-plug") then
+	os.execute("git clone -q https://github.com/jpnt/vis-plugged " ..
+	(os.getenv("XDG_CONFIG_HOME") or os.getenv("HOME") .. "/.config") ..
+	"/vis/plugins/vis-plugged") -- bootstrap vis-plugged
+end return require("plugins/vis-plugged") end)()
 
 plugged.add_plugin("https://github.com/lutobler/vis-commentary")
 plugged.add_plugin("https://git.sr.ht/~mcepl/vis-fzf-open")
 plugged.add_plugin("https://github.com/jpnt/vis-shout")
 plugged.add_plugin("https://github.com/kupospelov/vis-ctags")
 plugged.add_plugin("https://github.com/fischerling/vis-lspc")
---require("plugins/vis-autopairs") -- fix closing bracket edge case
---require("plugins/vis-bracketpaste") -- copy from vim bracketpaste
---require("plugins/vis-format") -- copformat plugin from neovim
+-- require("plugins/vis-bracketpaste") -- copy from vim bracketpaste
+-- require("plugins/vis-autopairs") -- fix closing bracket edge case
+-- require("plugins/vis-format") -- copformat plugin from neovim
 
 vis.events.subscribe(vis.events.INIT, function()
 	vis:command("set theme base16-gruvbox-dark-medium")
@@ -20,8 +24,8 @@ vis.events.subscribe(vis.events.WIN_OPEN, function(win)
 	vis:command("set relativenumbers")
 
 	local filetype = win.syntax or ""
-
-	if filetype == "javascript" or filetype == "typescript" then
+	if filetype == "javascript" or filetype == "typescript"
+	or filetype == "yaml"  then
 		vis:command("set expandtab on")
 		vis:command("set tabwidth 2")
 	elseif filetype == "python" or filetype == "rust" then
