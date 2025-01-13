1364312724e20aa02e3a21f24f9045a567f0d47e
Joao Pinto <joaopintojpgp@gmail.com>
2025-01-13 03:01:55 +0000

    visrc: add vis-autopairs plugin

--- vis/.config/vis/visrc.lua
+++ vis/.config/vis/visrc.lua
@@ -1,17 +1,19 @@
 require("vis")
-local plugged = (function() if not pcall(require, "plugins/vis-plugged") then
-	os.execute("git clone -q https://github.com/jpnt/vis-plugged " ..
-	(os.getenv("XDG_CONFIG_HOME") or os.getenv("HOME") .. "/.config") ..
-	"/vis/plugins/vis-plugged") -- bootstrap vis-plugged
-end return require("plugins/vis-plugged") end)()
+local plugged = (function() -- bootstrap vis-plugged
+	if not pcall(require, "plugins/vis-plugged") then
+		os.execute("git clone -q https://github.com/jpnt/vis-plugged " ..
+			(os.getenv("XDG_CONFIG_HOME") or os.getenv("HOME") .. "/.config") ..
+			"/vis/plugins/vis-plugged")
+	end
+	return require("plugins/vis-plugged")
+end)()
 
 plugged.add_plugin("https://github.com/lutobler/vis-commentary")
 plugged.add_plugin("https://git.sr.ht/~mcepl/vis-fzf-open")
 plugged.add_plugin("https://github.com/jpnt/vis-shout")
 plugged.add_plugin("https://github.com/kupospelov/vis-ctags")
 plugged.add_plugin("https://github.com/fischerling/vis-lspc")
--- require("plugins/vis-autopairs") -- fix closing bracket edge case
--- require("plugins/vis-format") -- copy format plugin from neovim
+plugged.add_plugin("https://github.com/jpnt/vis-autopairs")
 
 vis.events.subscribe(vis.events.INIT, function()
 	vis:command("set theme base16-gruvbox-dark-hard")
@@ -24,7 +26,7 @@ vis.events.subscribe(vis.events.WIN_OPEN, function(win)
 
 	local filetype = win.syntax or ""
 	if filetype == "javascript" or filetype == "typescript"
-	or filetype == "yaml"  then
+	    or filetype == "yaml" then
 		vis:command("set expandtab on")
 		vis:command("set tabwidth 2")
 	elseif filetype == "python" or filetype == "rust" then
