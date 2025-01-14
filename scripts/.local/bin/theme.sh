08cdf4e6c00198343883782e5353282486d40cc6
João Pinto <joaopintojpgp@gmail.com>
2025-01-14 18:43:09 +0000

    update theme.sh

--- scripts/.local/bin/theme.sh
+++ scripts/.local/bin/theme.sh
@@ -4,29 +4,20 @@ GTK_THEME="Numix"
 QT5_THEME="Fusion"
 QT6_THEME="Fusion"
 
+set -e
+
 set_gtk_theme() {
-	if command -v gsettings > /dev/null; then
-		gsettings set org.gnome.desktop.interface gtk-theme "$GTK_THEME"
-		gsettings set org.gnome.desktop.interface icon-theme "$GTK_THEME"
-		echo "(LOG) GTK theme applied: $GTK_THEME"
-	else
-		echo "(WARN) GTK settings not applied: unsupported environment"
-	fi
+	gsettings set org.gnome.desktop.interface gtk-theme "$GTK_THEME"
+	gsettings set org.gnome.desktop.interface icon-theme "$GTK_THEME"
+	echo "(LOG) GTK theme applied: $GTK_THEME"
 }
 
 set_qt_theme() {
-	if command -v qt5ct > /dev/null; then
-		qt5ct --style "$QT5_THEME"
-		echo "(LOG) QT5 theme applied: $QT5_THEME"
-	else
-		echo "(WARN) QT5 settings not applied: unsupported environment"
-	fi
+	qt5ct --style "$QT5_THEME"
+	echo "(LOG) QT5 theme applied: $QT5_THEME"
 
-	if command -v qt6ct > /dev/null; then
-		qt6ct --style "$QT6_THEME"
-	else
-		echo "(WARN) QT6 settings not applied: unsupported environment"
-	fi
+	qt6ct --style "$QT6_THEME"
+	echo "(LOG) QT6 theme applied: $QT6_THEME"
 }
 
 set_gtk_theme
