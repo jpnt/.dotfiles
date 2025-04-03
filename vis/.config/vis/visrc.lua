4ad3a576f001ca83880bfa98af1bd969ae3a14e7
João Pinto <joaopintojpgp@gmail.com>
2025-04-03 21:08:37 +0100

    update visrc,jpstack. add xdg-open

--- vis/.config/vis/visrc.lua
+++ vis/.config/vis/visrc.lua
@@ -1,12 +1,21 @@
 require("vis")
 local plugged = (function() -- bootstrap vis-plugged
-	if not pcall(require, "plugins/vis-plugged") then
-		os.execute("git clone -q https://github.com/jpnt/vis-plugged " ..
-			(os.getenv("XDG_CONFIG_HOME") or os.getenv("HOME") .. "/.config") ..
-			"/vis/plugins/vis-plugged")
-	end
-	return require("plugins/vis-plugged")
+  if not pcall(require, "plugins/vis-plugged") then
+    os.execute("git clone -q https://github.com/jpnt/vis-plugged " ..
+      (os.getenv("XDG_CONFIG_HOME") or os.getenv("HOME") .. "/.config") ..
+      "/vis/plugins/vis-plugged")
+  end
+  return require("plugins/vis-plugged")
 end)()
+
+local tab_settings = {
+  javascript = 2,
+  typescript = 2,
+  lua = 2,
+  yaml = 2,
+  python = 4,
+  rust = 4,
+}
 
 plugged.add_plugin("https://github.com/lutobler/vis-commentary")
 plugged.add_plugin("https://git.sr.ht/~mcepl/vis-fzf-open")
@@ -17,29 +26,25 @@ plugged.add_plugin("https://github.com/jpnt/vis-autopairs")
 plugged.require_all_plugins()
 
 vis.events.subscribe(vis.events.INIT, function()
-	--vis:command("set theme base16-brewer")
+  vis:command("set theme base16-brewer")
 end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
-	vis:command("set autoindent on")
-	vis:command("set number")
-	vis:command("set relativenumbers")
+  vis:command("set autoindent on")
+  vis:command("set number")
+  vis:command("set relativenumbers")
 
-	local filetype = win.syntax or ""
-	if filetype == "javascript" or filetype == "typescript"
-	    or filetype == "yaml" then
-		vis:command("set expandtab on")
-		vis:command("set tabwidth 2")
-	elseif filetype == "python" or filetype == "rust" then
-		vis:command("set expandtab on")
-		vis:command("set tabwidth 4")
-	end
+  local tabwidth = tab_settings[win.syntax or ""]
+  if tabwidth then
+    vis:command("set expandtab on")
+    vis:command("set tabwidth " .. tabwidth)
+  end
 end)
 
 vis.events.subscribe(vis.events.FILE_SAVE_PRE, function(file, path)
-	if not path then return end
-	local ext = path:match("^.+(%..+)$")
-	if ext and (ext == ".rs" or ext == ".go" or ext == ".py") then
-		vis:command("lspc-format")
-	end
+  if not path then return end
+  local ext = path:match("^.+(%..+)$")
+  if ext and (ext == ".rs" or ext == ".go" or ext == ".py" or ext == ".lua") then
+   vis:command("lspc-format")
+  end
 end)
