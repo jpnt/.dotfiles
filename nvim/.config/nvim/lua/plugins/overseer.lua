4487ec913900f98139d7243aba0c3bdb8df40d00
João Pinto <joaopintojpgp@gmail.com>
2025-05-19 11:46:40 +0100

    update nvim plugins

--- nvim/.config/nvim/lua/plugins/overseer.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "stevearc/overseer.nvim",
-  cmd = { "OverseerRun", "OverseerToggle" },
-  keys = { { "<leader>cc", "<cmd>OverseerRun<cr>" },
-    { "<leader>cu", "<cmd>OverseerToggle<cr>" } },
-  opts = {},
-}
