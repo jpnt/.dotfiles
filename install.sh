5942bf59a07ecb5fb665a46d24bd4ee14d17384a
João Pinto <joaopintojpgp@gmail.com>
2025-09-20 00:14:25 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 [ $# -eq 1 ] || { echo "usage: $0 <dir>"; exit 1; }
 [ -d "$1" ] || { echo "error: $1 not found or is not a directory"; exit 1; }
 
-src="$1"
+src="${1%/}" # strip trailing slash
 
 find "$src" -type f -printf '%P\n' | while read -r rel_path; do
     target_path="$HOME/$rel_path"
