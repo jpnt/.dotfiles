d89bdd544decdbadef8c8752b1b339afd637f931
João Pinto <joaopintojpgp@gmail.com>
2025-10-01 01:42:05 +0100

    nvim optimizations and change theme

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -37,46 +37,51 @@ vim.pack.add({
   { src = "https://github.com/vladdoster/remember.nvim" },
   { src = "https://github.com/L3MON4D3/LuaSnip" },
   { src = "https://github.com/rafamadriz/friendly-snippets" },
-  { src = "https:://github.com/xiyaowong/transparent.nvim" },
   { src = "https://github.com/dstein64/vim-startuptime" },
+  { src = "https://github.com/mg979/vim-visual-multi" },
   { src = "https://codeberg.org/mfussenegger/nvim-jdtls" },
   { src = "https://codeberg.org/mfussenegger/nvim-dap.git" },
 })
 
--- Plugin config/enable
-require("mini.icons").setup()
-require("mini.pick").setup()
-require("mini.ai").setup()
-require("mini.surround").setup()
-require("mini.tabline").setup()
-require("mini.statusline").setup()
-require("mini.diff").setup()
-require("mini.sessions").setup()
-require("mini.notify").setup()
-vim.notify = require("mini.notify").make_notify()
-require("guess-indent").setup()
-require("remember")
-require("luasnip.loaders.from_vscode").lazy_load()
+-- Theme
 require("mini.base16").setup({
   palette = {
-    base00 = "#000000",
-    base01 = "#161b22",
-    base02 = "#21262d",
-    base03 = "#484f58",
-    base04 = "#6e7681",
-    base05 = "#c9d1d9",
-    base06 = "#ecf2f8",
-    base07 = "#ffffff",
-    base08 = "#ff7b72",
-    base09 = "#d29922",
-    base0A = "#e3b341",
-    base0B = "#7ee787",
-    base0C = "#79c0ff",
-    base0D = "#a5d6ff",
-    base0E = "#d2a8ff",
-    base0F = "#ffa198",
+    base00 = "#2E3440",
+    base01 = "#3B4252",
+    base02 = "#434C5E",
+    base03 = "#4C566A",
+    base04 = "#D8DEE9",
+    base05 = "#E5E9F0",
+    base06 = "#ECEFF4",
+    base07 = "#ECEFF4",
+    base08 = "#BF616A",
+    base09 = "#D08770",
+    base0A = "#EBCB8B",
+    base0B = "#A3BE8C",
+    base0C = "#88C0D0",
+    base0D = "#81A1C1",
+    base0E = "#B48EAD",
+    base0F = "#D08770",
   },
 })
+
+-- Plugin config/enable
+vim.schedule(function() -- Do not lock
+  require("mini.icons").setup()
+  require("mini.pick").setup()
+  require("mini.ai").setup()
+  require("mini.surround").setup()
+  require("mini.tabline").setup()
+  require("mini.statusline").setup()
+  require("mini.diff").setup()
+  require("mini.sessions").setup()
+  require("mini.notify").setup()
+  vim.notify = require("mini.notify").make_notify()
+
+  require("luasnip.loaders.from_vscode").lazy_load()
+  require("guess-indent").setup()
+  require("remember")
+end)
 
 -- Keymaps
 vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
@@ -106,29 +111,26 @@ vim.keymap.set({ 'i', 's' }, '<C-e>', function()
   end
 end)
 
--- LSP
-vim.lsp.enable({
-  "clangd",
-  "lua_ls",
-  "ts_ls",
-  "pyright",
-  "gopls",
-  "rust_analyzer",
-  "zls",
-  "typst_lsp",
-  "jdtls",
-})
-
 vim.diagnostic.config({
   virtual_lines = { current_line = true },
 })
 
--- Autocmd
+-- LSP (Language Server Protocol)
+vim.api.nvim_create_autocmd({"BufReadPost", "BufNewFile"}, {
+  once = true,
+  callback = function()
+    -- vim.lsp.config is handled by nvim-lspconfig plugin
+    vim.lsp.enable({
+      "clangd", "lua_ls", "ts_ls", "pyright",
+      "gopls", "rust_analyzer", "zls", "typst_lsp", "jdtls"
+    })
+  end
+})
+
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(ev)
     local bufnr  = ev.buf
     local client = vim.lsp.get_client_by_id(ev.data.client_id)
-
     -- native LSP completion (omnifunc + <C-Space>)
     if client:supports_method("textDocument/completion") then
       vim.lsp.completion.enable(true, client.id, bufnr, { autotrigger = true })
@@ -137,7 +139,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
         vim.lsp.completion.get()
       end, { buffer = bufnr })
     end
-
     -- LSP specific keymaps
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr })
     vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr })
