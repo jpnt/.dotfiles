3b5a669ebc839dc03218ecefe71a56ddab64b2b0
João Pinto <joaopintojpgp@gmail.com>
2025-11-21 19:08:04 +0000

    flatpak theme

---
+++ flatpak-theme.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+flattak override --user --filesystem=xdg-data/themes
+flatpak override --user --env=GTK_THEME=Adwaita:dark
+flatpak override --user --env=QT_STYLE_OVERRIDE=Adwaita-Dark
--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # TODO: do not hardcode $HOME/.config/... as the XDG_CONFIG?
-# TODO: do not hardcode font across multiple files?
-# TODO: dbus session/flatpak dark theme
- 
+# TODO: glob/multiple file input support. ingore install.sh (itself) and README.md
+# TODO: do not hardcode font across multiple files? FONT flag to configure. default will be the one i use but also support doing e.g. FONT=... ./install.sh *   the command then will be simple just a sed of the default font for the font that was selected. of course we must validate if the font was changed that the font is valid!
+
 [ $# -eq 1 ] || { echo "usage: $0 <dir>"; exit 1; }
 [ -d "$1" ] || { echo "error: $1 not found or is not a directory"; exit 1; }
 
